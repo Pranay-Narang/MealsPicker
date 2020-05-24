845095575ae700c845a5a7474e29ffba29c740e5
Pranay-Narang <pranay@firehound.org>
2020-05-25 03:25:10 +0530

    lib: Hook grid items to launch another screen

---
+++ lib/meals.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class MealsScreen extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('The Recipies'),
+      ),
+      body: Center(
+        child: Text("Recipes Here"),
+      ),
+    );
+  }
+}
