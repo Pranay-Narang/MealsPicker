3c81f2903ce90bc2c550e0f2d0a246a3b92ef752
Pranay-Narang <pranay@firehound.org>
2020-05-25 02:29:08 +0530

    lib: Insert dummy objects into the grid layout
    * Also add some basic styling to the grid

--- lib/categories.dart
+++ lib/categories.dart
@@ -1,14 +1,24 @@
 import 'package:flutter/material.dart';
 
+import './dummy_data.dart';
+import './cgitems.dart';
+
 class Categories extends StatelessWidget {
   Widget build(BuildContext context) {
-    return GridView(
-      children: <Widget>[],
-      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-        maxCrossAxisExtent: 200,
-        childAspectRatio: 3/2,
-        crossAxisSpacing: 20,
-        mainAxisSpacing: 20,
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Meals App'),
+      ),
+      body: GridView(
+        children: DUMMY_CATEGORIES
+            .map((e) => CategoryItems(e.title, e.color))
+            .toList(),
+        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
       ),
     );
   }
