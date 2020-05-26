b8ea67f09bf365cd81845cb2befc3e885900e94d
Pranay-Narang <pranay@firehound.org>
2020-05-27 01:36:18 +0530

    filters: Wrap with scaffold
    That looked bad for sure

--- lib/filters.dart
+++ lib/filters.dart
@@ -2,6 +2,13 @@ import 'package:flutter/material.dart';
 
 class FiltersScreen extends StatelessWidget {
   Widget build(BuildContext context) {
-    return Center(child: Text('Filters Man'),);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Filters'),
+      ),
+      body: Center(
+        child: Text('Filters Man'),
+      ),
+    );
   }
 }
