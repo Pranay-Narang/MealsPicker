61792ab70fcaab8673313a5521bc9195fb3df4e0
Pranay-Narang <pranay@firehound.org>
2020-05-26 17:20:10 +0530

    meal_details: Show ingredients and steps

--- lib/meal_details.dart
+++ lib/meal_details.dart
@@ -1,13 +1,93 @@
 import 'package:flutter/material.dart';
 
+import './dummy_data.dart';
+
 class MealDetails extends StatelessWidget {
+  Widget buildSectionTitle(BuildContext context, String text) {
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 10),
+      child: Text(
+        text,
+        style: Theme.of(context).textTheme.headline6,
+      ),
+    );
+  }
+
+  Widget buildContainer({Widget child}) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.blueGrey,
+        borderRadius: BorderRadius.circular(15),
+      ),
+      height: 200,
+      width: 300,
+      margin: EdgeInsets.all(10),
+      padding: EdgeInsets.all(10),
+      child: child,
+    );
+  }
+
   Widget build(BuildContext context) {
     final mealId = ModalRoute.of(context).settings.arguments as String;
 
+    final selectedMeal =
+        DUMMY_MEALS.firstWhere((element) => element.id == mealId);
+
     return Scaffold(
-      appBar: AppBar(title: Text(mealId)),
-      body: Center(
-        child: Text("Meal Details Screen"),
+      appBar: AppBar(
+        title: Text('${selectedMeal.title}'),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Container(
+              height: 300,
+              width: double.infinity,
+              child: Image.network(
+                selectedMeal.imageUrl,
+                fit: BoxFit.cover,
+              ),
+            ),
+            buildSectionTitle(context, 'Ingredients'),
+            buildContainer(
+              child: ListView.builder(
+                  itemBuilder: (context, scrollIndex) => Card(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(15),
+                        ),
+                        color: Theme.of(context).accentColor,
+                        child: Padding(
+                          padding: EdgeInsets.all(5),
+                          child: Text(
+                            selectedMeal.ingredients[scrollIndex],
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ),
+                      ),
+                  itemCount: selectedMeal.ingredients.length),
+            ),
+            buildSectionTitle(context, 'Steps'),
+            buildContainer(
+              child: ListView.builder(
+                itemBuilder: (context, scrollIndex) => Column(
+                  children: <Widget>[
+                    ListTile(
+                      leading: CircleAvatar(
+                        child: Text('# ${scrollIndex + 1}'),
+                      ),
+                      title: Text(
+                        selectedMeal.steps[scrollIndex],
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                    Divider(),
+                  ],
+                ),
+                itemCount: selectedMeal.steps.length,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
