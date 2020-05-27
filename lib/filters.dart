7e43427fee4d8fc339e935aed5ec7e51fc7b0784
Pranay-Narang <pranay@firehound.org>
2020-05-28 00:38:56 +0530

    filters: Add basic switches and options
    - Currently logicless

--- lib/filters.dart
+++ lib/filters.dart
@@ -8,15 +8,84 @@ class FiltersScreen extends StatefulWidget {
 }
 
 class _FiltersScreenState extends State<FiltersScreen> {
+  var glutenFree = false;
+  var vegetarian = false;
+  var vegan = false;
+  var lactoseFree = false;
+
+  Widget buildSwitchListTile(
+      String title, String subtitle, bool currentVal, Function updateVal) {
+    return SwitchListTile(
+      title: Text(title),
+      value: currentVal,
+      subtitle: Text(subtitle),
+      onChanged: updateVal,
+    );
+  }
+
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Filters'),
       ),
-      body: Center(
-        child: Text('Filters Man'),
-      ),
       drawer: MainDrawer(),
+      body: Column(
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.all(20),
+            child: Text(
+              'Adjust meal selection',
+              style: Theme.of(context).textTheme.headline6,
+            ),
+          ),
+          Expanded(
+            child: ListView(
+              children: <Widget>[
+                buildSwitchListTile(
+                  'Gluten Free',
+                  'Include only "Gluten Free" meals',
+                  glutenFree,
+                  (newVal) {
+                    setState(() {
+                      glutenFree = newVal;
+                    });
+                  },
+                ),
+                buildSwitchListTile(
+                  'Lactose Free',
+                  'Include only "Lactose Free" meals',
+                  lactoseFree,
+                  (newVal) {
+                    setState(() {
+                      lactoseFree = newVal;
+                    });
+                  },
+                ),
+                buildSwitchListTile(
+                  'Vegetarian',
+                  'Include only vegetarian meals',
+                  vegetarian,
+                  (newVal) {
+                    setState(() {
+                      vegetarian = newVal;
+                    });
+                  },
+                ),
+                buildSwitchListTile(
+                  'Vegan',
+                  'Only include vegan meals',
+                  vegan,
+                  (newVal) {
+                    setState(() {
+                      vegan = newVal;
+                    });
+                  },
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
